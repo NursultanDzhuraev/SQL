348c44d35e3cfb1707ab3a67c752a81339016d17
nursultan <nursultsnjuraev95@gmail.com>
2025-01-13 16:37:22 +0600

    add teble terminl

--- src/terminal.sql
+++ src/terminal.sql
@@ -1,10 +1,15 @@
--- postgres=# create table Countrys(
---                                     postgres(# id serial primary key ,
---                                     name varchar(50) not null,
---                                     code varchar unique);
--- CREATE TABLE
---     postgres=#
 -- postgres=#
+-- create table Countrys
+-- (
+--     postgres(# id serial primary key,
+--     name varchar(50) not null,
+--     code varchar unique
+-- );
+-- CREATE TABLE
+
+
+
+
 -- create table Cities
 -- (
 --     postgres(# id serial primery key,
@@ -41,6 +46,10 @@
 --     code          int unique,
 --     people_amount int
 -- );
+
+
+
+
 -- CREATE TABLE
 -- postgres=#
 -- create table region
@@ -49,11 +58,19 @@
 --     postgres(# name varchar (50),
 --     postgres(# countrys_id int references countrys(id)
 -- );
+
+
+
+
 -- CREATE TABLE
 -- postgres=#
 -- alter table cities
 --     add column region_id int references region (id);
 -- ALTER TABLE
+
+
+
+
 -- postgres=#
 -- create table village
 -- (
@@ -63,3 +80,54 @@
 --     postgres(# region_id int references region(id)
 -- );
 -- CREATE TABLE;
+
+
+
+
+
+-- postgres=#
+-- create table cafe
+-- (
+--     postgres(# id serial primary key,
+--     postgres(# name varchar (50) not null,
+--     postgres(# address varchar (50)
+--         postgres(# date_of_brith date);
+-- ERROR:  syntax error at or near "date_of_brith"
+-- LINE 5: date_of_brith date);
+-- ^
+-- postgres=#
+-- create table cafe
+-- (
+--     id            serial primary key,
+--     name          varchar(50) not null,
+--     address       varchar(50),
+--     date_of_brith date
+-- );
+
+
+
+
+-- CREATE TABLE
+-- postgres=#
+-- create table waiters
+-- (
+--     postgres(# id serial primary key,
+--     postgres(# last_name varchar,
+--     postgres(# fist_name varchar not null,
+--     postgres(# gender varchar
+-- );
+-- CREATE TABLE
+
+
+
+
+
+-- postgres=#
+-- create table bartender
+-- (
+--     postgres(# id serial primary key,
+--     postgres(# last_name varchar (50),
+--     postgres(# first_name varchar (50) not null,
+--     postgres(# experience int
+-- );
+-- CREATE TABLE
